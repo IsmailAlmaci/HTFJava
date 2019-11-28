da018460a9520ab1d448f8eaa1ccee55b26d8f30
Ismail <s104428@ap.be>
2019-11-28 11:52:30 +0100

    finished get

--- src/main/java/com/kdgboys/HTF/Controller/AccountController.java
+++ src/main/java/com/kdgboys/HTF/Controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.kdgboys.HTF.Controller;
 
 import com.kdgboys.HTF.model.AccountDto;
+import com.kdgboys.HTF.model.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,18 +10,19 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class AccountController {
 
-    private static AccountDto getJson()
+    private static Page<AccountDto[]> getJson()
     {
         RestTemplate restTemplate = new RestTemplate();
+        Page<AccountDto> account = new Page<>();
         String url
-                = "http://vault.bewire.org/accounts/39831414-919b-4660-bb82-db1a043e8af2";
-        ResponseEntity<AccountDto> response
-                = restTemplate.getForEntity(url, AccountDto.class);
+                = "http://vault.bewire.org/accounts";
+        ResponseEntity<Page<AccountDto[]>> response
+                = restTemplate.getForEntity(url, (Class<Page<AccountDto[]>>)account.getClass());
         return response.getBody();
     }
 
     @GetMapping(value = "/account")
-    public AccountDto account() {
+    public Page account() {
         return getJson();
     }
 }
